61bba146e5fdae287f325c61d9b3024a85a6278f
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-09-01 19:50:19 +0530

    Update L.java

--- app/src/main/java/websank/corp/mahisan/techcrunch/L.java
+++ app/src/main/java/websank/corp/mahisan/techcrunch/L.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 /**
- * Created by user on 02-03-2015.
+ * Created by user on 02-03-2015
  */
 public class L {
     public static void m(String message)
